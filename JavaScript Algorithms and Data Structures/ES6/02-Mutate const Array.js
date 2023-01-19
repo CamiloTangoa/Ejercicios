d78581ff5d093eb2db3b5c085e89237fd3766e28
Camilo Tangoa <camiloandrestangoa@gmail.com>
2023-01-18 23:38:11 -0500

    Añade los ejercicios de ES6
    Añade los ejercicios pertenecientes a la seccion ES6 de freeCodeCamp https://www.freecodecamp.org/learn/javescript-algorithms-and-data-structures/

---
+++ JavaScript Algorithms and Data Structures/ES6/02-Mutate const Array.js
@@ -0,0 +1,8 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+} //Code of "Get a Hint", was trying with a for loop since i saw that as a automatic sorting but guess this is the right thing
+editInPlace();
