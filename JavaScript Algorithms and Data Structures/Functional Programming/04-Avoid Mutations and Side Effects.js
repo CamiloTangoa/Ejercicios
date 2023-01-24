0d37c725fbbcd599b995ec934e79df437b0abe56
CamiloTangoa <89042732+CamiloTangoa@users.noreply.github.com>
2023-01-23 23:10:45 -0500

    Añade los ejercicios de Functional Programming
    Añade los ejercicios pertenecientes a la seccion Functional Programming de freeCodeCamp https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/04-Avoid Mutations and Side Effects.js
@@ -0,0 +1,9 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+    let incremented = fixedValue + 1;
+    return incremented;
+  // Only change code above this line
+}
